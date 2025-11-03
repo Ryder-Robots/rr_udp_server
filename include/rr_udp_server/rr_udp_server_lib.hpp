4773515e428222b2d1ed040c53e9770f82d579cc
Aaron Spiteri <azzmosphere@gmail.com>
2025-11-03 17:27:34 +1100

    updated UDP servoce

--- include/rr_udp_server/rr_udp_server_lib.hpp
+++ include/rr_udp_server/rr_udp_server_lib.hpp
@@ -2,13 +2,16 @@
 #define RR_UDP_SERVER__RR_UDP_SERVER_LIB_HPP_
 
 #include <memory>
-
+#include <functional>
 #include "rclcpp/rclcpp.hpp"
 #include "rr_udp_server/visibility_control.h"
 #include "udp_msgs/msg/udp_packet.hpp"
 
 namespace rr_udp_server
 {
+
+#define RR_UDP_SVR_MSG_CLASS udp_msgs::msg::UdpPacket
+#define RR_UDP_SVR_READ_TOPIC "/udp_read"
 
 class RrUdpServerLib : public rclcpp::Node
 {
@@ -20,7 +23,7 @@ class RrUdpServerLib : public rclcpp::Node
    *
    * listens for
    */
-  void subscriber(const udp_msgs::msg::UdpPacket::SharedPtr packet);
+  void subscriber(const RR_UDP_SVR_MSG_CLASS::SharedPtr packet);
 
   void publisher();
 
@@ -28,8 +31,8 @@ class RrUdpServerLib : public rclcpp::Node
 
  private:
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<udp_msgs::msg::UdpPacket>::SharedPtr publisher_;
-  rclcpp::Subscription<udp_msgs::msg::UdpPacket>::SharedPtr subscription_;
+  rclcpp::Publisher<RR_UDP_SVR_MSG_CLASS>::SharedPtr publisher_;
+  rclcpp::Subscription<RR_UDP_SVR_MSG_CLASS>::SharedPtr subscription_;
   rclcpp::Logger logger_ = rclcpp::get_logger("rr_udp_server_lib");
 
   // private methods
--- src/rr_udp_server_lib.cpp
+++ src/rr_udp_server_lib.cpp
@@ -4,7 +4,8 @@ namespace rr_udp_server
 {
 
 void RrUdpServerLib::init() {
-
+    auto topic_callback =  std::bind(&RrUdpServerLib::subscriber, this, std::placeholders::_1);
+    subscription_ = this->create_subscription<RR_UDP_SVR_MSG_CLASS>(RR_UDP_SVR_READ_TOPIC, 10, topic_callback);
 }
 
 RrUdpServerLib::~RrUdpServerLib() {}
@@ -13,7 +14,8 @@ RrUdpServerLib::~RrUdpServerLib() {}
  * Read udp messages
  */
 void RrUdpServerLib::subscriber(const udp_msgs::msg::UdpPacket::SharedPtr packet) {
-    RCLCPP_DEBUG(logger_, "received message image msg");
+    char* char_ptr = reinterpret_cast<char*>(packet->data.data());
+    RCLCPP_DEBUG(logger_, "received message UDP msg: %s", char_ptr);
 }
 
 void RrUdpServerLib::publisher() {}
